25df6ff86d3f544b06c7fdb4d081173f64659e4d
JTM77XY <jeantales773@gmail.com>
2026-07-21 22:21:41 -0300

    atualização

--- cadastro de login/script.js
+++ cadastro de login/script.js
@@ -1,3 +1,38 @@
+// let cadastro = []
+
+// const dados = {
+//      nome: 'jtm',
+//      email: 'jean@gmail.com',
+//      senha: 123,
+// }
+// cadastro.push(dados)
+// console.log(cadastro)
+
+
+// function login(){
+//  const dadoslogin = {
+//      nome: document.getElementById('input-usuario').value,
+//      email: document.getElementById('input-email').value,
+//      senha: document.getElementById('input-senha').value,
+//  }
+
+//  for(let i = 0; i<cadastro.length; i++){
+//    if(dadoslogin.nome == cadastro[i].nome && dadoslogin.email == cadastro[i].email && dadoslogin.senha == cadastro[i].senha){
+//     window.location.href = "https://store.steampowered.com/?l=portuguese"
+//    }else{
+//     alert('asseso negado')
+//    }
+
+//  }
+// }
+
+
+
+
+
+
+
+
 let cadastro = []
 
 const dados = {
@@ -12,12 +47,11 @@ console.log(cadastro)
 function login(){
  const dadoslogin = {
      nome: document.getElementById('input-usuario').value,
-     email: document.getElementById('input-email').value,
      senha: document.getElementById('input-senha').value,
  }
 
  for(let i = 0; i<cadastro.length; i++){
-   if(dadoslogin.nome == cadastro[i].nome && dadoslogin.email == cadastro[i].email && dadoslogin.senha == cadastro[i].senha){
+   if(dadoslogin.nome == cadastro[i].nome || dadoslogin.nome == cadastro[i].email && dadoslogin.senha == cadastro[i].senha){
     window.location.href = "https://store.steampowered.com/?l=portuguese"
    }else{
     alert('asseso negado')
